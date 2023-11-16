e9b076bd951b09acb6ee5e181581c5704383b050
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-11-16 15:06:39 +0900

    feat: React.js 설치 React.js 설치 성공

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          React 설치 성공
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
