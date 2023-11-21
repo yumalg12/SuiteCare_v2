ceca147dc94c3cc33b3dc6db25dd4e0838943140
Dojin Park <coah2013@naver.com>
2023-11-21 18:21:03 +0900

    [refactor] SpringSecurity AutoConfig 임시 비활성화
    - 로그인 페이지 작성 완료 후 CustomConfig 파일 작성하여 구현 예정

--- src/main/java/com/suitecare/suitecare/SuitecareApplication.java
+++ src/main/java/com/suitecare/suitecare/SuitecareApplication.java
@@ -2,8 +2,9 @@ package com.suitecare.suitecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SuitecareApplication {
 
 	public static void main(String[] args) {
