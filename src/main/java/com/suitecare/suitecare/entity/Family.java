66212fdfc1e6b97636afd76b59ecd58658a0978b
DojinP <coah2013@naver.com>
2023-11-23 16:24:35 +0900

    [feat] SuiteFamily Entity 추가
    - SuiteFamily Entity 추가

---
+++ src/main/java/com/suitecare/suitecare/entity/Family.java
@@ -0,0 +1,27 @@
+package com.suitecare.suitecare.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.NonNull;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Family {
+    @NonNull
+    private String suite_family_id;
+    private String password;
+    private String family_name;
+    private String family_gender;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private Date family_birthday;
+    private String email;
+    private String tel;
+    private String address;
+    private String address_detail;
+    private String sms_opt_in;
+    private String email_opt_in;
+    @JsonFormat(pattern = "yyyy.MM.dd'T'hh:mm", timezone = "Asia/Seoul")
+    private Timestamp signup_date;
+    private String role;
+
+}
