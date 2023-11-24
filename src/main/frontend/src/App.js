b51b33dd971d1d4347223f44015039ec303be0bb
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-11-25 02:57:34 +0900

    [feat] 메인 페이지 틀 추가
    Header만 넣어둔 Family, Mate 메인 페이지 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,6 +5,8 @@ import FamilyLogin from './pages/Family/Login';
 import MateLogin from './pages/Mate/Login';
 import FamilySignUp from './pages/Family/SignUp';
 import MateSignUp from './pages/Mate/SignUp';
+import FamilyMain from './pages/Family/Main';
+import MateMain from './pages/Mate/Main';
 import Test from './pages/Test';
 
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
             <Route path='/mate/login' element={<MateLogin />} />
             <Route path='/family/signup' element={<FamilySignUp />} />
             <Route path='/mate/signup' element={<MateSignUp />} />
+            <Route path='/family/main' element={<FamilyMain />} />
+            <Route path='/mate/main' element={<MateMain />} />
             <Route path='/test' element={<Test/>}/>
           </Routes>
         </Router>
---
+++ src/main/frontend/src/pages/Mate/Main.js
@@ -0,0 +1,10 @@
+import Header from "../../component/Mate/Header/Header";
+
+const Main = () => {
+  return (<div className="Main">
+    <Header/>
+
+  </div>);
+}
+
+export default Main;
