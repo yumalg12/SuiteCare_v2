28dd47f0efa9580896da5ad6c70860841d76fe19
DojinP <coah2013@naver.com>
2023-11-25 12:39:43 +0900

    [feat] Login API 작성
    - LoginDTO 작성
    - LoginService 작성

--- src/main/java/com/suitecare/suitecare/api/service/LoginService.java
+++ src/main/java/com/suitecare/suitecare/api/service/LoginService.java
@@ -1,8 +1,18 @@
 package com.suitecare.suitecare.api.service;
 
+import com.suitecare.suitecare.api.repository.FamilyMapper;
+import com.suitecare.suitecare.domain.LoginDTO;
+import com.suitecare.suitecare.domain.FamilyDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
+    @Autowired
+    FamilyMapper familyMapper;
+
+    public FamilyDTO createFamily(LoginDTO login){
+        return familyMapper.login(login);
+    }
 }
--- src/main/java/com/suitecare/suitecare/domain/LoginDTO.java
+++ src/main/java/com/suitecare/suitecare/domain/LoginDTO.java
@@ -1,4 +1,9 @@
 package com.suitecare.suitecare.domain;
 
+import lombok.Data;
+
+@Data
 public class LoginDTO {
+    private String suite_family_id;
+    private String password;
 }
