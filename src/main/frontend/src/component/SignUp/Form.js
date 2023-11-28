8822bfb840c72e24bca9b4f13c10b73739545b9f
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-11-29 03:37:40 +0900

    [feat] 회원가입 페이지 작성
    - SignUp 페이지 수정됨
    - SignUp/Form 컴포넌트 추가됨

---
+++ src/main/frontend/src/component/SignUp/Form.js
@@ -0,0 +1,165 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
+const Form = ({ type }) => {
+  
+  //type에 따라 변경될 값을 모아 둔 함수
+  const valueSet = ($type) => {
+        if ($type === 'family') {
+            return {
+              url: '/family/signup',
+                buttonText: '패밀리 회원가입',
+                radioRedirect: '/mate/signup',
+                otherType: '메이트 (간병인)',
+            };
+          } else if ($type === 'mate') {
+            return {
+              url: '/mate/signup',
+                buttonText: '메이트 회원가입',
+                radioRedirect: '/family/signup',
+                otherType: '패밀리 (간병 서비스 이용자)',
+            };
+          }
+        };
+        
+    //회원가입 버튼 클릭 시 이동시키는 함수
+    const navigate = useNavigate();
+    const handleSignUpClick = () => {
+        const url = valueSet(type).url;
+        navigate(url);
+    };
+
+    //회원구분 부분의 라디오 버튼 클릭 시 페이지를 이동시키는 함수
+    const handleRadioClick = () => {
+        const url = valueSet(type).radioRedirect;
+        const otherType = valueSet(type).otherType;
+        if (window.confirm(otherType + ' 회원가입 페이지로 이동하시겠습니까?\n단, 기존 작성내역은 초기화됩니다.')) {
+            navigate(url);
+        }
+    };
+
+    //핸드폰 번호를 작성하고 hidden input에 모으는 파트
+    const [phoneParts, setPhoneParts] = useState({
+        phone_1: '',
+        phone_2: '',
+        phone_3: '',
+    });
+
+    const handlePhoneChange = ($event) => {
+        const { id, value } = $event.target;
+
+        const isValidInput = /^\d{0,4}$/.test(value);
+
+        if (isValidInput) {
+            setPhoneParts((prevPhoneParts) => ({
+                ...prevPhoneParts,
+                [id]: value,
+            }));
+        } else {
+            alert('숫자만 입력할 수 있습니다.');
+        }
+    };
+
+    const { phone_1, phone_2, phone_3 } = phoneParts;
+    const phoneNumber = `${phone_1}-${phone_2}-${phone_3}`;
+
+    //form에 포함될 input을 편하게 작성하기 위한 정보 모음
+    const formInputInfos = {
+        id: {
+            label: '아이디',
+            type: 'text',
+            name: 'id',
+            id: 'id',
+            maxLength: 20,
+        },
+        pw: {
+            label: '비밀번호',
+            type: 'password',
+            name: 'pw',
+            id: 'pw',
+            maxLength: 20,
+        },
+        pw_check: {
+            label: '비밀번호 확인',
+            type: 'password',
+            name: null,
+            id: 'pw_check',
+            maxLength: 20,
+        },
+        user_name: {
+            label: '성함',
+            type: 'text',
+            name: 'user_name',
+            id: 'user_name',
+            maxLength: 15,
+        },
+        phone_authentication: {
+            label: '인증번호',
+            type: 'text',
+            name: null,
+            id: 'phone_authentication',
+            maxLength: 5,
+        },
+    };
+
+    const formInputs = ($typeName) => {
+        return (
+            <div className="input_wrapper">
+                <label>{formInputInfos[$typeName].label}</label>
+                <input
+                    type={formInputInfos[$typeName].type}
+                    placeholder={formInputInfos[$typeName].label}
+                    name={formInputInfos[$typeName].name}
+                    id={formInputInfos[$typeName].id}
+                    maxLength={formInputInfos[$typeName].maxLength}
+                />
+            </div>
+        );
+    };
+
+    //렌더링 부분
+    return (
+        <div className="Form">
+          <h1>{valueSet(type).buttonText}</h1>
+            <div className="input_wrapper">
+                <label>회원 구분</label>
+                <div className="input_radio">
+                    <div className="radio_user_type">
+                        <input type="radio" value="family" checked={type === 'family'} onChange={handleRadioClick} />
+                        <span>패밀리 회원 (간병 서비스 이용자)</span>
+                    </div>
+                    <div className="radio_user_type">
+                        <input type="radio" value="mate" checked={type === 'mate'} onChange={handleRadioClick} />
+                        <span>메이트 회원 (간병인)</span>
+                    </div>
+                </div>
+            </div>
+
+            <hr />
+
+          <form name='signup'>
+            {formInputs('id')}
+            {formInputs('pw')}
+            {formInputs('pw_check')}
+            {formInputs('user_name')}
+
+            <div className="input_wrapper">
+                <label>휴대전화</label>
+                <div className="input_phone">
+                    <input type="text" placeholder="010" id="phone_1" value={phone_1} maxLength={3} onChange={handlePhoneChange} />-
+                    <input type="text" placeholder="0000" id="phone_2" value={phone_2} maxLength={4} onChange={handlePhoneChange} />-
+                    <input type="text" placeholder="0000" id="phone_3" value={phone_3} maxLength={4} onChange={handlePhoneChange} />
+                    <input type="hidden" name="phone" value={phoneNumber} />
+                </div>
+            </div>
+
+            {formInputs('phone_authentication')}
+
+            <hr />
+            <button onClick={handleSignUpClick}>{valueSet(type).buttonText}</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
