7d8cb3d32bb992104ff4b2332e2f8eac0d663d0e
Dojin Park <coah2013@naver.com>
2023-11-29 16:26:44 +0900

    [modify] 회원가입 회원 생성(createFamily) API 리턴타입 수정
    - 회원가입 회원 생성(createFamily) API 리턴타입 수정

--- src/main/java/com/suitecare/suitecare/api/repository/FamilyMapper.java
+++ src/main/java/com/suitecare/suitecare/api/repository/FamilyMapper.java
@@ -1,11 +1,12 @@
 package com.suitecare.suitecare.api.repository;
 
+import com.suitecare.suitecare.domain.CreateFamilyDTO;
 import com.suitecare.suitecare.domain.FamilyDTO;
 import com.suitecare.suitecare.domain.LoginDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FamilyMapper {
-    FamilyDTO createFamily(FamilyDTO familyDTO);
+    int createFamily(CreateFamilyDTO createFamilyDTO);
     FamilyDTO login(LoginDTO loginDTO);
 }
