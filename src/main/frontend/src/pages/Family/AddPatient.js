3f85413ff9f31a294bbf7d492eeda3c6f4f1af16
Lee Jae Sun <ze_s96@naver.com>
2023-11-30 12:27:48 +0900

    [feat] 환자등록페이지 작성(AddPatient)
    - css 추후 적용 필요

---
+++ src/main/frontend/src/pages/Family/AddPatient.js
@@ -0,0 +1,11 @@
+import Header from '../../component/Family/Header/Header';
+import Form from '../../component/Family/AddPatient/Form';
+
+function AddPatient() {
+    return <div>
+        <Header/>
+        <Form />
+    </div>
+};
+
+export default AddPatient;
