f7641b43b0ad6474208bfd3a981f3b78cd0adf21
Malg Eum Yu <134472216+yumalg12@users.noreply.github.com>
2023-11-30 11:47:54 +0900

    [feat] Login 페이지 수정
    - 로그인 페이지 및 Form.js 수정

--- src/main/frontend/src/pages/Family/Login.js
+++ src/main/frontend/src/pages/Family/Login.js
@@ -1,17 +1,11 @@
 import Form from '../../component/Login/Form'
 import Header from '../../component/Family/Header/Header'
-import FamilyDropdown from '../../component/Family/Header/Dropdown'
-
 
 function Login() {
   return (<div className="Login">
   <Header/>
-  <FamilyDropdown/>
-  <Form/>
-
+  <Form type='family'/>
   </div>)
 }
-
-
 
 export default Login;
--- src/main/frontend/src/pages/Mate/Login.js
+++ src/main/frontend/src/pages/Mate/Login.js
@@ -4,11 +4,8 @@ import Header from '../../component/Mate/Header/Header'
 function Login() {
   return (<div className="Login">
   <Header/>
-  <Form/>
-
+  <Form type='mate'/>
   </div>)
 }
-
-
 
 export default Login;
